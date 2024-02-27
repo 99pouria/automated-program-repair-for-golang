fb4b6920304c685d463d6b80e8eb71a4ff591a8b
Pooria Kalantari <46835187+pooria1@users.noreply.github.com>
2024-02-27 10:01:02 +0330

    call make env in main func

--- cmd/go_apr/main.go
+++ cmd/go_apr/main.go
@@ -5,19 +5,30 @@ import (
 	"fmt"
 
 	makebuildenv "github.com/99pouria/go-apr/internal/make_build_env"
+	preprocess "github.com/99pouria/go-apr/internal/pre-process"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	fileName := flag.String("p", "", "Path to golang file that contains function for test")
 	funcName := flag.String("f", "", "Name of function which needs repair")
 	testFile := flag.String("t", "", "Path to test files which contains test cases for given Golang function")
-	_ = testFile
+
 	flag.Parse()
 
-	if err := makebuildenv.MakeBuildEnv(*fileName, *funcName); err != nil {
-		fmt.Println(err)
-		return
+	fmt.Printf("Checking go file...\t\t")
+
+	goCode, err := preprocess.StartPreProcess(*fileName, *funcName, *testFile)
+	if err != nil {
+		logrus.WithField("error", err).Fatal("pre process failed")
 	}
 
-	// strconv.ParseInt()
+	fmt.Printf("OK\n")
+	fmt.Printf("Creating build environment...\t")
+
+	if err := makebuildenv.MakeBuildEnv(*goCode); err != nil {
+		logrus.WithField("error", err).Fatal("make build env failed")
+	}
+
+	fmt.Printf("OK\n")
 }
