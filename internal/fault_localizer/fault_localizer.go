00660e07fe5b0c2e25ed7198c22fb159759069e4
Pooria Kalantari <99pouria@gmail.com>
2024-03-22 18:25:00 +0330

    improve mechanisem of fault localizer

--- internal/fault_localizer/fault_localizer.go
+++ internal/fault_localizer/fault_localizer.go
@@ -3,22 +3,46 @@ package fl
 import (
 	"slices"
 
-	"github.com/99pouria/go-apr/internal/fault_localizer/faults"
+	issuetracker "github.com/99pouria/go-apr/internal/fault_localizer/issue-tracker"
 	"github.com/99pouria/go-apr/internal/projectenv"
-	"github.com/sirupsen/logrus"
+	"github.com/99pouria/go-apr/pkg/logger"
 )
 
-func LocalizeFaults(env *projectenv.Environment) {
-	f := faults.GetFaults(env)
+func LocalizeFaults(env *projectenv.Environment) (isFixed bool) {
+	issues := issuetracker.GetIssues(env)
 
 	var (
-		execError error
-		done      bool = false
+		fixedIssues []issuetracker.Issue
 	)
+	isFixed = false
 
-	for !done || execError != nil {
-		logrus.Info("Running testcases to find failed ones")
+	logger.PrintInBoxCenter("Program repair statarted", 46)
 
+	defer func() {
+		logger.PrintInBoxCenter("Program repair finished", 46)
+
+		switch len(fixedIssues) {
+		case 0:
+			logger.Println("No bugs found for code.")
+			if isFixed {
+				logger.Printf("All testcases passed. Code is %s\n", logger.Green("BUG-FREE"))
+			} else {
+				logger.Printf("Testcases execution %s. Can not repair.\n", logger.Red("FAILED"))
+			}
+		default:
+			logger.Println("Following bugs fixed:")
+			for index, f := range fixedIssues {
+				logger.Printf("\t[%s] %s\n", logger.Green(index+1), f.Description())
+			}
+			if isFixed {
+				logger.Printf("Repaired code passes all tests. Code is %s\n", logger.Green("BUG-FREE"))
+			} else {
+				logger.Printf("Repaired code passes more tests, but some still %s.\n", logger.Red("FAIL"))
+			}
+		}
+	}()
+
+	for {
 		// running testcases for 100 times
 		results := env.RunTestCases(false, 100)
 		var failedTestcases []int
@@ -29,73 +53,70 @@ func LocalizeFaults(env *projectenv.Environment) {
 		}
 
 		if len(failedTestcases) == 0 {
-			done = true
-			continue
-		}
-	}
-
-	for _, fault := range f {
-
-		ok, err := fault.Check()
-		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"error": err,
-				"fault": fault.Description(),
-			}).Error("Check failed")
-			continue
-		}
-
-		if ok {
-			logrus.WithField("fault", fault.Description()).Info("No fault")
-			continue
-		}
-
-		logrus.WithField("fault", fault.Description()).Warn("Fault detected, Trying to fix fault")
-
-		// store testcases that failed
-		results := env.RunTestCases(false, 100)
-		var failedTestcases []int
-		for _, result := range results {
-			if !result.Ok {
-				failedTestcases = append(failedTestcases, result.ID)
-			}
-		}
-
-		if err := fault.Fix(); err != nil {
-			logrus.WithField("fault", fault.Description()).Info("Can not fix fault")
-			// trying to revet changes applied by Fix method
-			fault.Revert()
-		}
-
-		// rerun with testcases TODO: import comment and refactor variables names
-		results = env.RunTestCases(false, 100)
-		var newFailedTestcases []int
-		for _, result := range results {
-			if !result.Ok {
-				newFailedTestcases = append(newFailedTestcases, result.ID)
-			}
-		}
-
-		// check result
-		improved := false
-
-		if len(newFailedTestcases) == 0 {
-			logrus.WithField("fault", fault.Description()).Info("All testcases passes!")
+			isFixed = true
 			return
 		}
 
-		for _, id := range newFailedTestcases {
-			if !slices.Contains(failedTestcases, id) {
+		for {
+			if !issues.Next() {
+				return
+			}
+			issue := issues.Get()
+
+			ok, err := issue.Check()
+			if err != nil {
+				logger.Printf("%s Can not check for bugs.\t%s=%s\n", logger.Red("[ERROR]"), logger.Yellow("Description"), issue.Description())
+				continue
+			}
+
+			if ok {
+				logger.Debugf("Issue checked and the code doesn't have it.\t%s=%s\n", logger.Yellow("Description"), issue.Description())
+				continue
+			}
+
+			logger.Printf("%s Bug detected. Trying to fix it.\t\t%s=%s\n", logger.Blue("[INFO]"), logger.Yellow("Bug description"), issue.Description())
+
+			if err := issue.Fix(); err != nil {
+				logger.Printf("%s Can not fix bug.\n", logger.Red("[ERROR]"))
+				// trying to revet changes applied by Fix method
+				if err := issue.Revert(); err != nil {
+					logger.Debugf("can not revert applied changes %s\n", err.Error())
+				}
+				continue
+			}
+
+			// running testcases 100 times after applying patch to check if code has improved or not
+			results = env.RunTestCases(true, 100)
+			var newFailedTestcases []int
+			for _, result := range results {
+				if !result.Ok {
+					newFailedTestcases = append(newFailedTestcases, result.ID)
+				}
+			}
+
+			// TODO: design a better mechanisem to check if code has improved or not
+			improved := false
+			for _, id := range newFailedTestcases {
+				if !slices.Contains(failedTestcases, id) {
+					improved = true
+				}
+			}
+
+			if len(failedTestcases) > len((newFailedTestcases)) {
 				improved = true
 			}
-		}
 
-		if improved {
-			logrus.WithField("fault", fault.Description()).Info("Some testcases passes after using the patch")
-		} else {
-			logrus.WithField("fault", fault.Description()).Warn("Patch wasn't effective. Reverting changes")
-			// TODO: handle error
-			fault.Revert()
+			if improved {
+				logger.Printf("%s More testcases passes now. Applying patch was effective.\n", logger.Blue("[INFO]"))
+				fixedIssues = append(fixedIssues, issue)
+				logger.Printf("%s Rerunning testcases to make sure there isn't any other bugs.\n", logger.Blue("[INFO]"))
+				break
+			} else {
+				logger.Printf("%s Applying patch was not effective. Reverting changes.\n", logger.Yellow("[WARN]"))
+				if err := issue.Revert(); err != nil {
+					logger.Debugf("Can not revert changes that applied: %s\n", err.Error())
+				}
+			}
 		}
 	}
 }
